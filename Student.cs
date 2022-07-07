4b995f52440b5dad74d2e55f7b4b7cee26841a1f
mtaqiyuddin <mtaqiyuddin101@gmail.com>
2022-07-07 13:25:38 +0700

    latihan Inheritance

---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    public class Student : Person
+    {
+        public string StudentId { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    public class Teacher : Person
+    {
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+    }
+}
